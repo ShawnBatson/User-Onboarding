072cb8288e5871362212c2e71e3a8c539d9ea2d6
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-01-23 20:13:51 -0600

    form complete, working on styling and return

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -5,6 +5,11 @@ import Axios from "axios";
 
 const NewUser = ({ errors, touched, values, status }) => {
   const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    status && setUser(person => [...person, status]);
+  }, [status]);
+
   return (
     <div className="personForm">
       <h1>User Form</h1>
@@ -12,27 +17,32 @@ const NewUser = ({ errors, touched, values, status }) => {
         <Form>
           <label forHTML="name">
             Name:
-            <Field type="text" name="name" />
+            <Field type="text" name="name" value={values.name} />
+            {touched.name && errors.name && <p>{errors.name}</p>}
           </label>
 
           <label forHTML=" email">
             E-mail:
-            <Field type="text" name="email" />
+            <Field type="text" name="email" value={values.email} />
+            {touched.email && errors.email && <p>{errors.email}</p>}
           </label>
 
           <label forHTML="Password">
             Password:
-            <Field type="password" name="password" />
+            <Field type="password" name="password" value={values.password} />
+            {touched.password && errors.password && <p>{errors.password}</p>}
           </label>
 
           <label forHTML="termsAndConditions">
             Terms and Conditions
             <Field as="checkbox" name="terms" />
+            {touched.terms && errors.terms && <p>{errors.terms}</p>}
           </label>
 
           <button type="submit">Submit!</button>
         </Form>
       </div>
+      user.map(user => <div>{values.name}</div>)
     </div>
   );
 };
@@ -47,11 +57,29 @@ const formikUserForm = withFormik({
     };
   },
   validationSchema: Yup.object().shape({
-    name: Yup.string().required("please fill this in!"),
-    email: Yup.string().required("please fill this in!"),
-    password: Yup.string().required(),
-    termsAndConditions: Yup.bool()
-  })
+    name: Yup.string().required("I'd really like to know your name"),
+    email: Yup.string().required("It's annoying, but this is required"),
+    password: Yup.string()
+      .password()
+      .required("Don't forget to make a strong password!"),
+    termsAndConditions: Yup.bool().required(
+      "I know you didn't read this, so just check that you've read them anyway."
+    )
+  }),
+
+  handleSubmit(values, { setStatus, resetForm }) {
+    console.log("Submitting form:", values);
+
+    Axios.post("https://reqres.in/api/users", values)
+      .then(res => {
+        setStatus(res.data);
+        resetForm();
+        console.log("Success!", res.data);
+      })
+      .catch(err => {
+        console.log("You have encountered an error", err);
+      });
+  }
 })(NewUser);
 
 export default formikUserForm;
